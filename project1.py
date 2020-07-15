7a3707304cbfa8b8011a9b8d92670a844d38203f
drummerboix <55928979+drummerboix@users.noreply.github.com>
2020-07-15 05:05:11 -0400

    first commit

---
+++ project1.py
@@ -0,0 +1,44 @@
+# import library for random stuff
+from random import *
+# for letters stuff
+import string
+
+# make pre-requisites
+char1 = string.ascii_uppercase
+char2 = char1.lower()
+char3 = "0123456789"
+char4 = "!@#$%^&*()"
+
+# Welcome messages
+def message():
+    print("Hello man, let's get you a password")
+    print("Please select the length of your password")
+    print(userGen())
+
+# User inputs for all 4 elements
+def userGen():
+    upper = int(input("Input number of upper case characters: "))
+    lower = int(input("Input number of lower case characters: "))
+    spec = int(input("Input number of special characters: "))
+    num = int(input("Input number of integer characters: "))
+    return passGen(upper,lower,spec,num)
+
+# Generating passwords from random stuff
+def passGen(upper,lower,spec,num):
+    newpass = []
+    if (upper+lower+spec+num)<6:
+        print("Enter minimum 6 characters")
+    else:
+        for i in range(upper):
+            newpass += choice(char1)
+        for i in range(lower):
+            newpass += choice(char2)
+        for i in range(spec):
+            newpass += choice(char4)
+        for i in range(num):
+            newpass += choice(char3)
+    password = list(newpass)
+    shuff = shuffle(password)
+    acpass = "".join(password)
+    return acpass
+message()
---
+++ project2.py
@@ -0,0 +1,68 @@
+# for random generation
+from random import *
+
+# # your guess
+# user = int(input("Enter your guess: "))
+# # random number
+# prog_guess = randint(0,10)
+# run = True
+
+# def Guess_stuff():
+#     if prog_guess == user:
+#         print("Good job mate")
+#     elif prog_guess < user:
+#         print("Guess again mate, too high")
+#     else:
+#         print("Guess again mate, too low")
+
+#     print("The correct answer is %i" %prog_guess)
+
+# while run:
+#     Guess_stuff()
+
+# starters for the while loop
+original = 0
+# variable for while loop, accepts true or false
+run = True
+
+player_name = input("Hi human! What is your name? ")
+count = 0
+
+def Guess_stuff():
+    # pull original and run into the function
+    global original
+    global run
+    global count
+    # generate random number for the program
+    prog_guess = randint(0,20)
+    # empty user input
+    user = []
+    if original == 0:
+        if count == 0:
+            user = input("Ok %s, guess a number between 0 and 20: " %player_name)
+        else:
+            user = input("Try again: ")
+    else:
+        user = input(str(original))
+
+    # break the program if user enters quit
+    if user == "quit":
+        print("Goodbye human!")
+        run = False
+    else:
+        # if guess is correct
+        if prog_guess == int(user):
+            print("Good job mate!")
+        elif prog_guess < int(user):
+            print("Guess again mate, too high")
+        elif prog_guess > int(user):
+            print("Guess again mate, too low")
+        print("Correct answer is %i" %prog_guess)
+        count += 1
+
+    if count > 5:
+        print("Damn %s, you've done this %i times" %(player_name, count))
+
+while run:
+    Guess_stuff()
+
